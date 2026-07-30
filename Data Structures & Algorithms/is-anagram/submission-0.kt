3f54bb2b1ccba2d00bcda4edbbee245357eff36f
YashTailor <137476021+YashTaylor@users.noreply.github.com>
2026-07-30 10:59:42 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if(s.length != t.length) return false
+        var mapS = mutableMapOf<Char, Int>()
+        var mapT = mutableMapOf<Char, Int>()
+        for (i in s.indices)
+        {
+            mapS[s[i]] = mapS.getOrDefault(s[i], 0) + 1
+            mapT[t[i]] = mapT.getOrDefault(t[i], 0) + 1
+        }
+        return mapS == mapT
+    }
+}
